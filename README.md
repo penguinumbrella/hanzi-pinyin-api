64fe9a260f260170b75a1fa6642ce1854bd1fe04
Kurt <17579556+kurt213@users.noreply.github.com>
2023-12-13 14:02:24 +0800

    updated readme and DeepL translation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # hanzi-pinyin-api
 Create a personal REST API to translate Hanzi Chinese characters to Pinyin
+
+## About
+This is a simple REST API that translates Hanzi Chinese characters to Pinyin. It uses the following two sources:
+- [Pinyin Jyutping Python Library](https://github.com/Language-Tools/pinyin-jyutping)
+- [DeepL Translation API](https://www.deepl.com/pro-api?cta=header-pro-api). You need to setup a free account to get the API key
+
+## Installation
+1. Clone this repository
+2. Install the requirements: `pip install -r requirements.txt`
+3. Create a `.env` file in the root directory and add the following variables, replacing `<your-deepl-api-key>` with your own API key:
+```
+DEEPL_API_KEY=<your-deepl-api-key>
+```
+4. Run the app: `python main.py`
+5. The app will run by default on `http://127.0.0.1:8000`
+
+## Usage
+The API has two endpoints:
+- `/` - returns hello world
+- `/hanzi/:hanzi-string` - translates a Hanzi string to Pinyin
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
-from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
 
 import pinyin_jyutping
+import deepl
+
+load_dotenv()
+translator = deepl.Translator(os.getenv("DEEPL_API_KEY"))
 
 app = FastAPI()
 
@@ -16,14 +21,15 @@ def read_root():
 
 @app.get("/pinyin/{hanzi}", response_class=PlainTextResponse)
 def read_item(hanzi: Union[str, None]):
+
     p = pinyin_jyutping.PinyinJyutping()
-    #output_pinyin = p.pinyin(hanzi, tone_numbers=True, spaces=True)
     output_pinyin = p.pinyin(hanzi, spaces=True)
-    print(output_pinyin)
-    response = "{}, {}".format(hanzi, output_pinyin)
+    translation = translator.translate_text(hanzi, source_lang="ZH", target_lang="EN-GB")
+
+    response = """{}\n{}\n{}""".format(hanzi, output_pinyin, translation)
     return response
 
 if __name__ == "__main__":
     p = pinyin_jyutping.PinyinJyutping()
-    output_pinyin = p.pinyin("你好我是英國人")
+    output_pinyin = p.pinyin("你好")
     print(output_pinyin)
